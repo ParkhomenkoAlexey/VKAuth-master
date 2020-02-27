58277a76d74ffca6304e48be05e1ffca9db9b1fd
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2020-02-27 12:47:41 +0500

    2 урок final

--- VKAuth/AppDelegate.swift
+++ VKAuth/AppDelegate.swift
@@ -12,24 +12,17 @@ import VKSdkFramework
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
     
-//    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-//        VKSdk.processOpen(url, fromApplication: UIApplication.OpenURLOptionsKey.sourceApplication.rawValue)
-//        return true
-//    }
-    
-//    @available(iOS 9.0, *)
-//    func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any])
-//      -> Bool {
-//        VKSdk.processOpen(url, fromApplication: UIApplication.OpenURLOptionsKey.sourceApplication.rawValue)
-//      return true
-//    }
+    @available(iOS 9.0, *)
+    func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any])
+      -> Bool {
+        VKSdk.processOpen(url, fromApplication: UIApplication.OpenURLOptionsKey.sourceApplication.rawValue)
+      return true
+    }
 
     // MARK: UISceneSession Lifecycle
 
